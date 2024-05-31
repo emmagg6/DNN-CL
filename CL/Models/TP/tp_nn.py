c02abd424a22497de834e1938f854b6ecb1e3516
Emma <125744177+emmagg6@users.noreply.github.com>
2024-05-31 12:22:14 -0400

    updates to EP

---
+++ CL/Models/TP/tp_nn.py
@@ -0,0 +1,281 @@
+'''
+Based on the code used for the experiments conducted in the submitted paper 
+"Fixed-Weight Difference Target Propagation" by K. K. S. Wu, K. C. K. Lee, and T. Poggio.
+
+Adaptation for runs of Continual Learning by emmagg6.
+
+'''
+
+
+from Models.TP.tp_layers import tp_layer
+from Models.TP.net import net
+from Models.TP.tp_fcns import parameterized_function
+from utils import calc_angle
+from copy import deepcopy
+
+import sys
+import time
+import wandb
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd.functional import jacobian
+import os
+import json
+
+
+class tp_net(net):
+    def __init__(self, depth, direct_depth, in_dim, hid_dim, out_dim, loss_function, device, params=None):
+        self.depth = depth
+        self.direct_depth = direct_depth
+        self.loss_function = loss_function
+        self.device = device
+        self.MSELoss = nn.MSELoss(reduction="sum")
+        self.layers = self.init_layers(in_dim, hid_dim, out_dim, params)
+        self.back_trainable = (params["bf1"]["type"] == "parameterized")
+
+    def init_layers(self, in_dim, hid_dim, out_dim, params):
+        layers = [None] * self.depth
+        dims = [in_dim] + [hid_dim] * (self.depth - 1) + [out_dim]
+        for d in range(self.depth - 1):
+            # print(f"Layer {d}: {dims[d]} -> {dims[d + 1]}")
+            # print(f"ff1: {params['ff1']}")
+            layers[d] = tp_layer(dims[d], dims[d + 1], self.device, params)
+        params_last = deepcopy(params)
+        params_last["ff2"]["act"] = params["last"]
+        layers[-1] = tp_layer(dims[-2], dims[-1], self.device, params_last)
+        return layers
+
+    def forward(self, x, update=True):
+        y = x
+        for d in range(self.depth):
+            y = self.layers[d].forward(y, update=update)
+        return y
+
+    def train(self, train_loader, valid_loader, epochs, lr, lrb, std, stepsize, log, save, hyperparams=None,
+              trial = 0, new_ckpt = '', train_ckpts = ''):
+        
+        train_losses = []
+        train_accuracies = []
+        test_losses = []
+        test_accuracies = []
+        eps = []
+
+        # Pre-train the feedback weights
+        for e in range(hyperparams["epochs_backward"]):
+            torch.cuda.empty_cache()
+            for x, y in train_loader:
+                x, y = x.to(self.device), y.to(self.device)
+                self.train_back_weights(x, y, lrb, std, loss_type=hyperparams["loss_feedback"])
+
+        # Train the feedforward and feedback weights
+        for e in range(epochs + 1):
+            print(f"Epoch: {e}")
+            torch.cuda.empty_cache()
+            start_time = time.time()
+            if e > 0:
+                for x, y in train_loader:
+                    x, y = x.to(self.device), y.to(self.device)
+                    # Train feedback weights
+                    for i in range(hyperparams["epochs_backward"]):
+                        self.train_back_weights(x, y, lrb, std, loss_type=hyperparams["loss_feedback"])
+                    # Train forward weights
+                    self.compute_target(x, y, stepsize)
+                    self.update_weights(x, lr)
+            end_time = time.time()
+
+            # Compute Positive semi-definiteness (the strict condition) and Trace (the weak condition)
+            eigenvalues_ratio = [torch.zeros(1, device=self.device) for d in range(self.depth)]
+            eigenvalues_trace = [torch.zeros(1, device=self.device) for d in range(self.depth)]
+            for x, y in valid_loader:
+                x, y = x.to(self.device), y.to(self.device)
+                with torch.no_grad():
+                    self.forward(x)
+                    for d in range(1, self.depth - self.direct_depth + 1):
+                        h1 = self.layers[d].input[0]
+                        gradf = jacobian(self.layers[d].forward, h1)
+                        h2 = self.layers[d].forward(h1)
+                        gradg = jacobian(self.layers[d].backward_function_1.forward, h2)
+                        eig, _ = torch.linalg.eig(gradf @ gradg)
+                        eigenvalues_ratio[d] += (eig.real > 0).sum() / len(eig.real)
+                        eigenvalues_trace[d] += torch.trace(gradf @ gradg)
+            for d in range(self.depth):
+                eigenvalues_ratio[d] /= len(valid_loader)
+                eigenvalues_trace[d] /= len(valid_loader)
+
+            # Predict
+            with torch.no_grad():
+                train_loss, train_acc = self.test(train_loader)
+                valid_loss, valid_acc = self.test(valid_loader)
+
+
+            train_losses.append(train_loss.item())
+            train_accuracies.append(train_acc)
+            test_losses.append(valid_loss.item())
+            test_accuracies.append(valid_acc)
+            eps.append(e)
+
+            # Logging
+            if log:
+                log_dict = {}
+                log_dict["train loss"] = train_loss
+                log_dict["valid loss"] = valid_loss
+                if train_acc is not None:
+                    log_dict["train accuracy"] = train_acc
+                if valid_acc is not None:
+                    log_dict["valid accuracy"] = valid_acc
+                log_dict["time"] = end_time - start_time
+                for d in range(1, self.depth - self.direct_depth + 1):
+                    log_dict[f"eigenvalue ratio {d}"] = eigenvalues_ratio[d].item()
+                    log_dict[f"eigenvalue trace {d}"] = eigenvalues_trace[d].item()
+
+                wandb.log(log_dict)
+
+            else:
+                if train_acc is not None:
+                    print(f"\tTrain Acc        : {train_acc}")
+                if valid_acc is not None:
+                    print(f"\tValid Acc        : {valid_acc}")
+
+        if save == 'yes':
+            self.save_model(new_ckpt)
+            self.save_training_dynamics(train_losses, train_accuracies, test_losses, test_accuracies, trial, train_ckpts)
+        
+
+    def train_back_weights(self, x, y, lrb, std, loss_type="DTP"):
+        if not self.back_trainable:
+            return
+        # print("loss_type: ", loss_type)
+        self.forward(x)
+        for d in reversed(range(1, self.depth - self.direct_depth + 1)):
+            if loss_type == "DTP":
+                q = self.layers[d - 1].output.detach().clone()
+                q = q + torch.normal(0, std, size=q.shape, device=self.device)
+                q_upper = self.layers[d].forward(q)
+                h = self.layers[d].backward_function_1.forward(q_upper)
+                loss = self.MSELoss(h, q)
+            else:
+                raise NotImplementedError()
+            self.layers[d].zero_grad()
+            loss.backward(retain_graph=True)
+            self.layers[d].update_backward(lrb / len(x))
+
+    def compute_target(self, x, y, stepsize):
+        y_pred = self.forward(x)
+        loss = self.loss_function(y_pred, y)
+        for d in range(self.depth):
+            self.layers[d].zero_grad()
+        loss.backward(retain_graph=True)
+
+        with torch.no_grad():
+            for d in range(self.depth - self.direct_depth, self.depth):
+                self.layers[d].target = self.layers[d].output - \
+                    stepsize * self.layers[d].output.grad
+
+            for d in reversed(range(self.depth - self.direct_depth)):
+                plane = self.layers[d + 1].backward_function_1.forward(self.layers[d + 1].target)
+                diff = self.layers[d + 1].backward_function_2.forward(plane, self.layers[d].output)
+                self.layers[d].target = diff
+
+    def update_weights(self, x, lr):
+        self.forward(x)
+        for d in range(self.depth):
+            loss = self.MSELoss(self.layers[d].target, self.layers[d].output)
+            self.layers[d].zero_grad()
+            loss.backward(retain_graph=True)
+            self.layers[d].update_forward(lr / len(x))
+
+    def get_layer_params(self):
+        layer_params = {}
+        for idx, layer in enumerate(self.layers):
+            layer_params[f'layer_{idx}'] = {
+                'forward_function_1': layer.forward_function_1.get_params(),
+                'forward_function_2': layer.forward_function_2.get_params(),
+                'backward_function_1': layer.backward_function_1.get_params(),
+                'backward_function_2': layer.backward_function_2.get_params(),
+            }
+        return layer_params
+
+    def save_model(self, ckpt):
+        path = ckpt
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        # Collect the parameters from each layer's functions
+        layer_params = {}
+        for idx, layer in enumerate(self.layers):
+            layer_params[f'layer_{idx}'] = {
+                'forward_function_1': layer.forward_function_1.get_params(),
+                'forward_function_2': layer.forward_function_2.get_params(),
+                'backward_function_1': layer.backward_function_1.get_params(),
+                'backward_function_2': layer.backward_function_2.get_params(),
+            }
+        # Save the collected parameters to the specified path
+        torch.save(layer_params, path)
+
+
+
+    def save_training_dynamics(self, train_losses, train_accuracies, test_losses, test_accuracies, trial, ckpt):
+        path = ckpt
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        # Check if the file exists and has content
+        if os.path.exists(path) and os.path.getsize(path) > 0:
+            with open(path, "r") as file:
+                data = json.load(file)
+        else:
+            # Initialize the dictionary with lists to store results
+            data = [{
+                "Trial": [],
+                "Train Losses": [],
+                "Train Accuracies": [],
+                "Test Losses": [],
+                "Test Accuracies": []
+            }]
+
+        # Append new results to each list within the first dictionary entry
+        data[0]["Trial"].append(trial)
+        data[0]["Train Losses"].append(train_losses)
+        data[0]["Train Accuracies"].append(train_accuracies)
+        data[0]["Test Losses"].append(test_losses)
+        data[0]["Test Accuracies"].append(test_accuracies)
+
+        # Write the updated dictionary back to the file
+        with open(path, "w") as file:
+            json.dump(data, file, indent=4)
+
+
+
+    def load_state(self, state_dict):
+        for layer_key, layer_state in state_dict.items():
+            layer_idx = int(layer_key.split('_')[-1])
+            layer = self.layers[layer_idx]
+
+            if 'forward_function_1' in layer_state and hasattr(layer.forward_function_1, 'load_params'):
+                layer.forward_function_1.load_params(layer_state['forward_function_1'])
+
+            if 'forward_function_2' in layer_state and hasattr(layer.forward_function_2, 'load_params'):
+                layer.forward_function_2.load_params(layer_state['forward_function_2'])
+
+            if 'backward_function_1' in layer_state and hasattr(layer.backward_function_1, 'load_params'):
+                layer.backward_function_1.load_params(layer_state['backward_function_1'])
+
+            if 'backward_function_2' in layer_state and hasattr(layer.backward_function_2, 'load_params'):
+                layer.backward_function_2.load_params(layer_state['backward_function_2'])
+
+        print("Model loaded successfully")
+
+    # def external_test(self, test_loader, state_dict=None):
+    #     if state_dict is not None:
+    #         self.load_state(state_dict)
+    #     loss = 0
+    #     correct = 0
+    #     total = 0
+    #     for x, y in test_loader:
+    #         x, y = x.to(self.device), y.to(self.device)
+    #         y_pred = self.forward(x)
+    #         loss += self.loss_function(y_pred, y).item()
+    #         _, predicted = y_pred.max(1)
+    #         total += y.size(0)
+    #         correct += predicted.eq(y).sum().item()
+    #     return loss / total, 100 * correct / total
+
+
---
+++ CL/utils.py
@@ -0,0 +1,386 @@
+
+####################################################################################################
+#                                           BP & TP                                                #
+####################################################################################################
+
+'''
+Following utils on code used for the experiments conducted in the submitted paper 
+"Fixed-Weight Difference Target Propagation" by K. K. S. Wu, K. C. K. Lee, and T. Poggio.
+
+'''
+
+import os
+import numpy as np
+import torch
+from torch import nn
+import math
+import wandb
+
+
+def combined_loss(pred, label, device="cpu", num_classes=10):
+    batch_size = pred.shape[0]
+    dim = pred.shape[1]
+    E = torch.eye(dim, device=device)
+    E1 = E[:, :num_classes]
+    E2 = E[:, num_classes:]
+    ce = nn.CrossEntropyLoss(reduction="sum")
+    mse = nn.MSELoss(reduction="sum")
+    return ce(pred @ E1, (label @ E1).max(axis=1).indices) + 1e-3 * mse(pred @ E2, label @ E2)
+
+
+def calc_accuracy(pred, label):
+    max_index = pred.max(axis=1).indices
+    return (max_index == label).sum().item() / label.shape[0]
+
+
+def calc_accuracy_combined(pred, label, num_classes=10):
+    data_size = pred.shape[0]
+    pred_max = pred[:, :num_classes].max(axis=1).indices
+    label_max = label[:, :num_classes].max(axis=1).indices
+    return (pred_max == label_max).sum().item() / data_size
+
+
+def set_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def worker_init_fn(worker_id):
+    np.random.seed(np.random.get_state()[1][0] + worker_id)
+
+
+def calc_angle(v1, v2):
+    cos = (v1 * v2).sum(axis=1) / (torch.norm(v1, dim=1) * torch.norm(v2, dim=1) + 1e-12)
+    cos = torch.clamp(cos, min=-1, max=1)
+    acos = torch.acos(cos) * 180 / math.pi
+    angle = 180 - torch.abs(acos - 180)
+    return angle
+
+
+def batch_normalization(x, mean=None, std=None):
+    if mean is None:
+        mean = torch.mean(x, dim=0)
+    if std is None:
+        std = torch.std(x, dim=0)
+    return (x - mean) / (std + 1e-12)
+
+
+def batch_normalization_inverse(y, mean, std):
+    return y * std + mean
+
+
+def set_wandb(args, params):
+    config = args.copy()
+    name = {"ff1": "forward_function_1",
+            "ff2": "forward_function_2",
+            "bf1": "backward_function_1",
+            "bf2": "backward_function_2"}
+    for n in name.keys():
+        config[name[n]] = params[n]["type"]
+        config[name[n] + "_init"] = params[n]["init"]
+        config[name[n] + "_activation"] = params[n]["act"]
+    config["last_activation"] = params["last"]
+    wandb.init(config=config)
+
+
+def set_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+        os.environ['OMP_NUM_THREADS'] = '1'
+    return device
+
+
+####################################################################################################
+#                                               PC                                                #
+####################################################################################################
+'''
+The following utils are used for the experiments conducted in the submitted paper:
+@article{millidge2020predictive,
+  title={Predictive Coding Approximates Backprop along Arbitrary Computation Graphs},
+  author={Millidge, Beren and Tschantz, Alexander and Buckley, Christopher L},
+  journal={arXiv preprint arXiv:2006.04182},
+  year={2020}
+}
+'''
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+import torch.distributions as dist
+from copy import deepcopy
+import math
+import matplotlib.pyplot as plt
+
+global DEVICE
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+### General Utils ###
+def boolcheck(x):
+    return str(x).lower() in ["true", "1", "yes"]
+
+def set_tensor(xs):
+    return xs.float().to(DEVICE)
+
+def edge_zero_pad(img,d):
+  N,C, h,w = img.shape
+  x = torch.zeros((N,C,h+(d*2),w+(d*2))).to(DEVICE)
+  x[:,:,d:h+d,d:w+d] = img
+  return x
+
+def accuracy(out, L):
+  B,l = out.shape
+  total = 0
+  for i in range(B):
+    if torch.argmax(out[i,:]) == torch.argmax(L[i,:]):
+      total +=1
+  return total/ B
+
+def sequence_accuracy(model, target_batch):
+    accuracy = 0
+    L = len(target_batch)
+    _,B = target_batch[0].shape
+    s = ""
+    for i in range(len(target_batch)): # this loop is over the seq_len
+      s += str(torch.argmax(model.mu_y[i][:,0]).item()) + " " + str(torch.argmax(target_batch[i][:,0]).item()) + "  "
+      for b in range(B):
+        #print("target idx: ", torch.argmax(target_batch[i][:,b]).item())
+        #print("pred idx: ", torch.argmax(model.mu_y[i][:,b]).item())
+        if torch.argmax(target_batch[i][:,b]) ==torch.argmax(model.mu_y[i][:,b]):
+          accuracy+=1
+    print("accs: ", s)
+    return accuracy / (L * B)
+
+def custom_onehot(idx, shape):
+  ret = set_tensor(torch.zeros(shape))
+  ret[idx] =1
+  return ret
+
+# def onehot(arr, vocab_size):
+#   print(arr)
+#   print(arr.type())
+#   L, B = arr.shape
+#   ret = np.zeros([L,vocab_size,B])
+#   for l in range(L):
+#     for b in range(B):
+#       ret[l,int(arr[l,b]),b] = 1
+#   return ret
+
+#####################################################################################################
+#                       (emmagg6) fix compatibility with custom onehot enc                          #
+#####################################################################################################
+def onehot(arr, vocab_size):
+    arr = arr.unsqueeze(1)  # Ensure arr is 2D: [N, 1]
+    one_hot = torch.zeros((arr.size(0), vocab_size), device=arr.device)  # Create a zero tensor of size [N, vocab_size]
+    one_hot.scatter_(1, arr, 1)  # Fill with 1s at the indices from arr
+    return one_hot
+
+def inverse_list_onehot(arr):
+  L = len(arr)
+  V,B = arr[0].shape
+  ret = np.zeros([L,B])
+  for l in range(L):
+    for b in range(B):
+      for v in range(V):
+        if arr[l][v,b] == 1:
+          ret[l,b] = v
+  return ret
+
+def decode_ypreds(ypreds):
+  L = len(ypreds)
+  V,B = ypreds[0].shape
+  ret = np.zeros([L,B])
+  for l in range(L):
+    for b in range(B):
+      v = torch.argmax(ypreds[l][:,b])
+      ret[l,b] =v
+  return ret
+
+
+def inverse_onehot(arr):
+  if type(arr) == list:
+    return inverse_list_onehot(arr)
+  else:
+    L,V,B = arr.shape
+    ret = np.zeros([L,B])
+    for l in range(L):
+      for b in range(B):
+        for v in range(V):
+          if arr[l,v,b] == 1:
+            ret[l,b] = v
+    return ret
+
+### Activation functions ###
+def tanh(xs):
+    return torch.tanh(xs)
+
+def linear(x):
+    return x
+
+def tanh_deriv(xs):
+    return 1.0 - torch.tanh(xs) ** 2.0
+
+def linear_deriv(x):
+    return set_tensor(torch.ones((1,)))
+
+def relu(xs):
+  return torch.clamp(xs,min=0)
+
+def relu_deriv(xs):
+  rel = relu(xs)
+  rel[rel>0] = 1
+  return rel
+
+# def softmax(xs):
+#   return F.softmax(xs)
+
+def sigmoid(xs):
+  return F.sigmoid(xs)
+
+def sigmoid_deriv(xs):
+  return F.sigmoid(xs) * (torch.ones_like(xs) - F.sigmoid(xs))
+
+
+### loss functions
+def mse_loss(out, label):
+      return torch.sum((out-label)**2)
+
+def mse_deriv(out,label):
+      return 2 * (out - label)
+
+ce_loss = nn.CrossEntropyLoss()
+
+def cross_entropy_loss(out,label):
+      return ce_loss(out,label)
+
+def my_cross_entropy(out,label):
+      return -torch.sum(label * torch.log(out + 1e-6))
+
+def cross_entropy_deriv(out,label):
+      return out - label
+
+def parse_loss_function(loss_arg):
+      if loss_arg == "mse":
+            return mse_loss, mse_deriv
+      elif loss_arg == "crossentropy":
+            return my_cross_entropy, cross_entropy_deriv
+      else:
+            raise ValueError("loss argument not expected. Can be one of 'mse' and 'crossentropy'. You inputted " + str(loss_arg))
+
+
+### Initialization Functions ###
+def gaussian_init(W,mean=0.0, std=0.05):
+  return W.normal_(mean=0.0,std=0.05)
+
+def zeros_init(W):
+  return torch.zeros_like(W)
+
+def kaiming_init(W, a=math.sqrt(5),*kwargs):
+  return init.kaiming_uniform_(W, a)
+
+def glorot_init(W):
+  return init.xavier_normal_(W)
+
+################# self is not defined this is an error - emmagg6 ######
+# def kaiming_bias_init(b,*kwargs):
+#   fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+#   bound = 1 / math.sqrt(fan_in)
+#   return init.uniform_(b, -bound, bound)
+
+#the initialization pytorch uses for lstm
+def std_uniform_init(W,hidden_size):
+  stdv = 1.0 / math.sqrt(hidden_size)
+  return init.uniform_(W, -stdv, stdv)
+
+
+
+
+#####################################################################################################
+#                                               KAN                                            #
+#####################################################################################################
+
+# from https://github.com/IvanDrokin/torch-conv-kan/blob/main/utils/regularization.py
+# Based on this implementations: https://github.com/szymonmaszke/torchlayers/blob/master/torchlayers/regularization.py
+import abc
+
+import torch
+import torch.nn as nn
+
+class WeightDecay(nn.Module):
+    def __init__(self, module, weight_decay, name: str = None):
+        if weight_decay < 0.0:
+            raise ValueError(
+                "Regularization's weight_decay should be greater than 0.0, got {}".format(
+                    weight_decay
+                )
+            )
+
+        super().__init__()
+        self.module = module
+        self.weight_decay = weight_decay
+        self.name = name
+
+        self.hook = self.module.register_full_backward_hook(self._weight_decay_hook)
+
+    def remove(self):
+        self.hook.remove()
+
+    def _weight_decay_hook(self, *_):
+        if self.name is None:
+            for param in self.module.parameters():
+                if param.grad is None or torch.all(param.grad == 0.0):
+                    param.grad = self.regularize(param)
+        else:
+            for name, param in self.module.named_parameters():
+                if self.name in name and (
+                    param.grad is None or torch.all(param.grad == 0.0)
+                ):
+                    param.grad = self.regularize(param)
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
+
+    def extra_repr(self) -> str:
+        representation = "weight_decay={}".format(self.weight_decay)
+        if self.name is not None:
+            representation += ", name={}".format(self.name)
+        return representation
+
+    @abc.abstractmethod
+    def regularize(self, parameter):
+        pass
+
+class L1(WeightDecay):
+    """Regularize module's parameters using L1 weight decay.
+
+    Example::
+
+        import torchlayers as tl
+
+        # Regularize all parameters of Linear module
+        regularized_layer = tl.L1(tl.Linear(30), weight_decay=1e-5)
+
+    .. note::
+            Backward hook will be registered on `module`. If you wish
+            to remove `L1` regularization use `remove()` method.
+
+    Parameters
+    ----------
+    module : torch.nn.Module
+        Module whose parameters will be regularized.
+    weight_decay : float
+        Strength of regularization (has to be greater than `0.0`).
+    name : str, optional
+        Name of parameter to be regularized (if any).
+        Default: all parameters will be regularized (including "bias").
+
+    """
+
+    def regularize(self, parameter):
+        return self.weight_decay * torch.sign(parameter.data)
