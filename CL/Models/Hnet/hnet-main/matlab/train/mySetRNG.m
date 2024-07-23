53eb4afac447af550e4ee662da8767f93e511432
Emma <125744177+emmagg6@users.noreply.github.com>
2024-07-23 14:01:14 -0400

    Hnet uploads and updates

---
+++ CL/Models/Hnet/hnet-main/matlab/train/mySetRNG.m
@@ -0,0 +1,11 @@
+%%% Random seed function
+
+function mySetRNG(cfg)
+    % Check if cfg is a structure with a field 'seed'
+    if isstruct(cfg) && isfield(cfg, 'seed')
+        rng(cfg.seed); % Initialize RNG with the provided seed
+        fprintf('RNG initialized to seed = %d\n', cfg.seed);
+    else
+        error('cfg must be a structure with a field ''seed''');
+    end
+end
